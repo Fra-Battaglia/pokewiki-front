be8ccef150d186bb479eeaf273d79fb31690b440
Fra-Battaglia <francescobattaglia03@gmail.com>
2025-01-02 23:13:04 +0100

    stats section added

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import Move from './Move';
 import Header from './header';
+import { width } from '@fortawesome/free-brands-svg-icons/fa42Group';
 
 function PokemonDetail(props) {
 	const [pokemon, set_pokemon] = useState(null);
@@ -40,18 +41,27 @@ function PokemonDetail(props) {
 						<div className="container my-8 mx-auto">
 							<div>
 								<Link to={-1}><button className="border border-white bg-black/[.4] flex gap-2 items-center justify-center self-center px-4 py-2 hover:underline" id='io'><FontAwesomeIcon icon={faArrowLeft} /> Back</button></Link>
-								<div className="flex items-start">
-									<div className="bg-black/[.4]"><img src={pokemon.sprites.other.home.front_default} alt={pokemon.name} /></div>
-									<div className="pokemon-info grow overflow-auto flex flex-col">
-										<h3 className='uppercase font-bold mb-4 text-6xl'>{pokemon.name}</h3>
-										<div className="types flex gap-4 mb-4">
+								<div className="flex items-start gap-4">
+									<div className="bg-black/[.4] shrink-0"><img src={pokemon.sprites.other.home.front_default} alt={pokemon.name} /></div>
+									<div className="pokemon-info grow overflow-auto flex flex-col gap-4">
+										<h3 className='uppercase font-bold mb-4 text-6xl text-center'>{pokemon.name}</h3>
+										<div className="types flex gap-4">
 										{pokemon.types.map((item) => (
 											<div className={"type uppercase rounded px-2 font-bold " + item.type.name} key={item.type.name}>{item.type.name}</div>
 										))}
 										</div>
-										<div className="stats">
+										<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, ducimus recusandae. Aspernatur deserunt vero vitae commodi autem repellat, vel id sed reprehenderit explicabo consectetur aliquid ex alias. Doloribus, praesentium facere?</p>
+										<h4 className='uppercase font-bold text-4xl text-center'>STATISTICS</h4>
+										<div className="stats grid grid-cols-3 gap-4">
 											{pokemon.stats.map((item) => (
-												<p key={item.stat.name}>{item.stat.name}: {item.base_stat}</p>
+												<>
+													<div className="stat">
+														<p key={item.stat.name}>{item.stat.name}: <strong>{item.base_stat}</strong></p>
+														<div className='bg-black/[.4] h-4'>
+															<div style={{width: item.base_stat / 255 * 100 + "%"}} className={"h-full bg-gradient-to-r " + (pokemon.types[0] ? `from-[--type-${pokemon.types[0].type.name}]` : "") + " " + (pokemon.types[1] ? `to-[--type-${pokemon.types[1].type.name}]` : pokemon.types[0] ? `to-[--type-${pokemon.types[0].type.name}]` : "")}></div>
+														</div>
+													</div>
+												</>
 											))}
 										</div>
 									</div>
